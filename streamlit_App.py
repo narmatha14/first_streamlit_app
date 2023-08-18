ec10c0e7c751b884ae53426bc70578d60e375254
narmatha14 <66859565+narmatha14@users.noreply.github.com>
2023-08-18 12:28:18 +0530

    Update streamlit_App.py
    fixed typo

--- streamlit_App.py
+++ streamlit_App.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My mom's new healthy dinner')
+streamlit.title('My moms new healthy dinner')
 
 streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')

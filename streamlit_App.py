0d9707ba69e34878d3d5e8252228f44696a2af10
narmatha14 <66859565+narmatha14@users.noreply.github.com>
2023-08-18 12:27:55 +0530

    Update streamlit_App.py
    Added header and mom's favorite items

--- streamlit_App.py
+++ streamlit_App.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My parents new healthy dinner')
+streamlit.title('My mom's new healthy dinner')
 
-streamlit.header('🥣 Breakfast Menu')
+streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
